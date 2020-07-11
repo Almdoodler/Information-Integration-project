2e141ddc8716b8d3471fce1b84855bb8c27ae9bb
Mezak <mezak@outlook.de>
2020-07-11 14:17:05 +0200

    Anpassungen an omdb interface

--- OMDB.py
+++ OMDB.py
@@ -1,39 +1,106 @@
 import requests
 from Movie import Movie
+import math
 
 
 
 class OMDB:
 
     search_url = 'https://www.omdbapi.com/?apikey=3c011de4&s='
-    movie_url = 'https://www.omdbapi.com/?apikey=3c011de4&t='
+    movie_url = 'https://www.omdbapi.com/?apikey=3c011de4&i='
 
     """ Gibt Ergebnisse für Film-Suchanfrage 'movieName' zurück. 
     return: movies (list<Movie>) """
     def search(self, movieName):
         r = requests.get(self.search_url + movieName)
         dic = r.json()
-        
-        if int(dic['totalResults'])>0:
+        movies = []
+
+        if dic['Response'] == "False":
+            print('Failure')
+
+        elif int(dic['totalResults'])>0:
+
             elements = self.cleanResult(dic['Search'], 'movie')
-            movies = self.createMovie(elements)
+            pages = math.ceil(int(dic['totalResults'])/10)
+            for i in range(2, pages):
+                r = requests.get(self.search_url + movieName + '&page=' + str(i))
+                elements = elements + self.cleanResult(r.json()['Search'], 'movie')
+            
+            for element in elements:
+                movies.append(self.createMovie(element))
 
             for movie in movies:
-                r = requests.get(self.movie_url + movie.getTitle())
+                r = requests.get(self.movie_url + movie.getImbID())
                 dic = r.json()
-                if dic['Response'] == 'True':
-                    actors = [actor.strip(' ') for actor in dic['Actors'].split(',')]
-                    movie.setActors(actors)
-                    movie.setProduction(dic['Production'])
-                    movie.setRuntime(dic['Runtime'])
+                production = False
+                runtime = False
+                acts = False
+                keys = dic.keys()
+
+                for key in keys:
+                    if key == "Production":
+                        production = True
+                        movie.setProduction(dic['Production'])
+                    elif key == "Runtime":
+                        runtime = True
+                        movie.setRuntime(dic['Runtime'])
+                    elif key == "Actors":
+                        acts = True
+                        actors = [actor.strip(' ') for actor in dic['Actors'].split(',')]
+                        movie.setActors(actors)
+                
+                if not production:
+                    movie.setProduction("N/A")
+                if not runtime:
+                    movie.setRuntime(0)
+                if not acts :
+                    movie.setActors([])
+                print(movie.string())
+        
+        return movies
+    
+
+    """ Gibt Ergebnis für Film-Suchanfrage 'id' zurück. 
+    return: movies (Movie)"""
+    def search_id(self, id):
+        r = requests.get(self.movie_url + id)
+        dic = r.json()
+        print(dic)
+        movies = []
+
+        if dic['Response'] == "False":
+            print('Failure')
 
         else:
-            movies = []
 
+            movie = self.createMovie(dic)
+            production = False
+            runtime = False
+            acts = False
+            keys = dic.keys()
 
-        for movie in movies:
-            print('Title' + movie.getTitle())
-            print('Actors', movie.getActors())
+            for key in keys:
+                if key == "Production":
+                    production = True
+                    movie.setProduction(dic['Production'])
+                elif key == "Runtime":
+                    runtime = True
+                    movie.setRuntime(dic['Runtime'])
+                elif key == "Actors":
+                    acts = True
+                    actors = [actor.strip(' ') for actor in dic['Actors'].split(',')]
+                    movie.setActors(actors)
+            
+            if not production:
+                movie.setProduction("N/A")
+            if not runtime:
+                movie.setRuntime(0)
+            if not acts :
+                movie.setActors([])
+            movies.append(movie)
+            print("movie ", movie.string())
+
         return movies
     
         
@@ -44,24 +111,49 @@ class OMDB:
     def cleanResult(self, dic, filter):
         delete = []
         for i in range(0, len(dic)):
-
             if dic[i]['Type'] != filter:
                 delete.append(i)
-        
+        delete.sort(reverse=True)
         for index in delete:
             del(dic[index])
 
         return dic
     
     
-    def createMovie(self, elements):
-        movies = []
-        for element in elements:
-            movies.append(Movie(element['Title'], element['Year'], element['imdbID']))
-        return movies
+    def createMovie(self, element):
+        
+        keys = element.keys()
+        con_title = False
+        con_year = False
+        con_imdbID = False
+        for key in keys:
+            if key == "Title":
+                title = element['Title']
+                con_title = True
+            elif key == "Year":
+                year = element['Year']
+                con_year = True
+            elif key == "imdbID":
+                imdbID = element['imdbID']
+                con_imdbID = True
+
+        
+        if not con_title:
+            title = 0
+        
+        if not con_year:
+            year = 0
+        
+        if not con_imdbID:
+            imdbID = -1
+
+        
+
+        return Movie(title, year, imdbID)
             
         
          
 if __name__ == "__main__":
     s = OMDB()
-    s.search('Batman')
+    s.search_id('tt8220550')
+    s.search_id('Naruto')
