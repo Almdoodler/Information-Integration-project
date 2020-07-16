4d3ee12389c006db2cd0d3f237d1b6a26a2ec38e
Markus Klenk <markus.klenk93@googlemail.com>
2020-07-16 20:50:16 +0200

    Main.py: ';' statt ',' als Trennzeichen, evtl. gibt es ja Filme die ein Komma im Title haben; Programm stürtzt bei falschen Eingaben jetzt nichtmehr ab.
    Signed-off-by: Markus Klenk <markus.klenk93@googlemail.com>

--- Main.py
+++ Main.py
@@ -8,19 +8,22 @@ class Main:
         print("Welcome to the Movie Integrator 3000")
         print("")
         print("Please enter")
-        print("If you want to search by movie name please enter '1, <your movie>' (Example: '1, John Wick)'")
-        print("If you want to search by imdb-ID please enter '2, <imdb-ID>' (Example: '2, tt2911666')")
+        print("If you want to search by movie name please enter '1; <your movie>' (Example: '1; John Wick)'")
+        print("If you want to search by imdb-ID please enter '2; <imdb-ID>' (Example: '2; tt2911666')")
+        # Probiere den String zu Teilen, wird ein falscher Separator, kein Separator oder zuviele Separators verwendet
+        # wird ValueError geworfen
+        try:
+            dir, input = input().split(';')
+            input = input.lstrip()
+            if dir == "1":
+                print("Processing..")
+                mediator.showData(input, "1")
 
-        dir, input = input().split(',')
-        input = input.lstrip()
-
-        if dir == "1":
-            print("Processing..")
-            mediator.showData(input, "1")
-
-        elif dir == "2":
-            print("Processing..")
-            mediator.showData(input, "2")
-
+            elif dir == "2":
+                print("Processing..")
+                mediator.showData(input, "2")
+        except ValueError:
+            print("Your input contained an invalid separator, no separator or multiple separators or contained no mode selector"
+                  ", allowed separators are ';'. Please try again! (ValueError)")
         else:
-            print("Your input was wrong.")
+            print('Your input contained an invalid mode selector, allowed values are 1 and 2. Please try again! (InvalidModeError)')
